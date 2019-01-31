821ca50f547ad6a1eece4a4e66e73349d76e358f
Guenter Bailey <office@bailey-solution.com>
2019-01-31 19:36:06 +0100

    rewrite speedtestcli-db main code and remove cdb.sql. add CREATEDB and CREATEView in main code. store database in subdirectory db to use in Docker as Volume. start add environment variable to use later in Docker and docker-compose scripts.

--- speedtestcli-db.py
+++ speedtestcli-db.py
@@ -2,8 +2,8 @@
 ###################################################
 # Apache License 2.0 Copyright 2017 Günter Bailey #
 ###################################################
-from os import curdir, sep
-import os
+from os import curdir, sep, getenv, mkdir, path, rename
+import json
 import sys
 import datetime
 import sqlite3 as lite
@@ -11,40 +11,51 @@ from subprocess import Popen, PIPE
 from optparse import OptionParser
 
 
-DEBUG = False
-DB_NAME="speedtestdb.db"
-prog_version = "0.1.3"
+DEBUG = True if getenv('debug', 'False') == 'True' else False
+DB_NAME = getenv('db_name', 'speedtestdb.db')
+DB_PATH = curdir+sep+'db'
+prog_version = '1.0'
+
+CREATE_DB = ['CREATE TABLE "speedtest" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "ping" FLOAT, "dl_speed" FLOAT, "up_speed" FLOAT, "timestamp" DATETIME DEFAULT CURRENT_TIMESTAMP, "ping_unit" INTEGER, "dl_unit" INTEGER, "up_unit" INTEGER);',
+             'CREATE TABLE "unit" ("id" INTEGER PRIMARY KEY  AUTOINCREMENT  NOT NULL , "name" TEXT NOT NULL  UNIQUE );']
+
+CREATE_VIEW = ['CREATE VIEW `group_date_min_view` AS select min(ping), min(dl_speed), min(up_speed), date(timestamp) from speedtest group by date(timestamp) order by date(timestamp);',
+               'CREATE VIEW `group_date_view` AS select ping, dl_speed, up_speed, date(timestamp) from speedtest group by date(timestamp) order by date(timestamp);',
+               'CREATE VIEW `max_group_date_view` AS select max(ping), max(dl_speed), max(up_speed), date(timestamp) from speedtest group by date(timestamp) order by date(timestamp);']
+
+
+def create_db_dir():
+    if not path.exists(DB_PATH):
+        mkdir(DB_PATH)
 
 
 def create_db():
     if DEBUG:
-        print("Create DB: "+str(DB_NAME))
-    con = lite.connect(curdir+sep+str(DB_NAME))
-    f = open(curdir+sep+"cdb.sql", 'r')
-    for x in f:
+        print("Create DB: {}".format(DB_NAME))
+    con = lite.connect(DB_PATH+sep+DB_NAME)
+    dbcreatelist = CREATE_DB + CREATE_VIEW
+    for x in dbcreatelist:
         con.execute(x)
     con.close()
-    f.close()
     if DEBUG:
         print("DB created")
 
 
 def create_new_db():
-    t = '{0:%H%M%S}'.format(datetime.datetime.now())
-    d = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
-    oldfile = curdir+sep+str(DB_NAME)
-    newfile = curdir+sep+str(d+t+"_"+DB_NAME)
-    if os.path.isfile(curdir+sep+str(DB_NAME)):
-        os.rename(oldfile, newfile)
-        print("rename current db to "+str(d+t+"_"+DB_NAME))
+    dt = '{0:%Y-%m-%d_%H%M%S}'.format(datetime.datetime.now())
+    oldfile = DB_PATH+sep+'{}'.format(DB_NAME)
+    newfile = DB_PATH+sep+'{}_{}'.format(dt, DB_NAME)
+    if path.isfile(DB_PATH+sep+str(DB_NAME)):
+        rename(oldfile, newfile)
+        print("rename current db to {}_{}".format(dt, DB_NAME))
 
-    if not os.path.isfile(curdir+sep+str(DB_NAME)):
+    if not path.isfile(DB_PATH+sep+'{}'.format(DB_NAME)):
         check_db()
 
 
-class database:
+class database():
     def __init__(self):
-        self.connection = lite.connect(curdir+sep+str(DB_NAME))
+        self.connection = lite.connect(DB_PATH+sep+'{}'.format(DB_NAME))
         self.cursor = self.connection.cursor()
 
     def insert(self, query):
@@ -71,47 +82,74 @@ class database:
 
 
 def check_db():
-    if not os.path.isfile(curdir+sep+str(DB_NAME)):
+    if not path.exists(DB_PATH):
+        create_db_dir()
+    if not path.isfile(DB_PATH+sep+'{}'.format(DB_NAME)):
         create_db()
 
 
 class Speedtest:
     def __init__(self):
-        self.dbname = DB_NAME
-        self.result = dict()
+        self.result = dict({'meta_server': dict(), 'meta_client': dict(), 'version': str(prog_version)})
         check_db()
         self.db = database()
+        self.msg = """Speedtestcli-db {version} Completed\n=========================\nResults
+-------------------------\nPing = {ping} {ping_unit}\nDownload = {dl_speed} {dl_unit}
+Upload = {up_speed} {up_unit}\n========================="""
+
+    def cv_humanreadable(self, size=0):
+        """
+        convert bit/s into human readable size
+        2**10 = 1024
+        :param size: bit/s float or integer
+        :return: tupple (<float round 2 decimal places>, <unit>)
+        """
+        power = 2**10
+        n = 0
+        powern = dict({0: 'bit/s', 1: 'Kbit/s', 2: 'Mbit/s', 3: 'Gbit/s', 4: 'Tbit/s'})
+        while size > power:
+            size /= power
+            n += 1
+        return '{0:.2f}'.format(size), powern.get(n, 'bit/s')
+
+    def print_msg(self):
+        """
+        format and print result
+        :return:
+        """
+        print(self.msg.format(**self.result))
 
     def speedtest(self):
         if DEBUG:
             print("start Speedtest")
-        cmd = ["speedtest-cli", "--simple"]
+        cmd = ["speedtest-cli", "--json"]
         output = Popen(cmd, stdout=PIPE, stderr=PIPE)
         out1, outerror = output.communicate()
         outdec = out1.decode()
-        for x in outdec.split('\n'):
-            try:
-                if DEBUG:
-                    print(x.split(':')[0].strip().lower(), " = ", x.split(':')[1].strip())
-                self.result.update({x.split(':')[0].strip().lower(): x.split(':')[1].strip()})
-            except IndexError:
-                pass
-
+        jsdata = outdec.split('\n')[0]
+        data = json.loads(jsdata)
+        self.result.update({'dl_speed': self.cv_humanreadable(size=data.get('download', 0))[0],
+                            'dl_unit': self.cv_humanreadable(size=data.get('download', 0))[1],
+                            'up_speed': self.cv_humanreadable(size=data.get('upload', 0))[0],
+                            'up_unit': self.cv_humanreadable(size=data.get('upload', 0))[1],
+                            'ping_unit': 'ms', 'ping': data.get('ping', -99)})
+        self.result['meta_server'] = data.get('server', '')
+        self.result['meta_client'] = data.get('client', '')
         self.save_todb()
 
     def get_or_set_unit(self, unit):
-        while True:
-            squery = """SELECT `unit`.`id` FROM `unit` WHERE `name`='%s';""" % (str(unit))
+        c = 0
+        while c < 2:
+            squery = """SELECT `unit`.`id` FROM `unit` WHERE `name`='{}';""".format(unit)
             d = self.db.idquery(squery)
-            if d != None:
+            if d:
                 return d[0]
             else:
-                ins = """INSERT INTO unit(name) VALUES ('%s');""" %(str(unit))
+                ins = """INSERT INTO unit(name) VALUES ('{}');""".format(unit)
                 self.db.insert(ins)
+            c += 1
 
     def save_todb(self):
-        t = '{0:%H:%M:%S}'.format(datetime.datetime.now())
-        d = '{0:%Y-%m-%d}'.format(datetime.datetime.now())
         if DEBUG:
             print("store into DB")
         if not len(self.result) >= 3:
@@ -119,30 +157,28 @@ class Speedtest:
                 print("No Result = ", self.result)
             exit(0)
 
-        ins = """INSERT INTO speedtest(ping, dl_speed, up_speed, date, time, ping_unit, dl_unit, up_unit) VALUES 
-('%s', '%s', '%s', '%s', '%s', '%s', '%s','%s');""" %(self.result["ping"].split(' ')[0],
-                                                      self.result["download"].split(' ')[0],
-                                                      self.result["upload"].split(' ')[0], d, t,
-                                                      self.get_or_set_unit(unit=(self.result["ping"].split(' ')[1])),
-                                                      self.get_or_set_unit(unit=(self.result["download"].split(' ')[1])),
-                                                      self.get_or_set_unit(unit=(self.result["upload"].split(' ')[1])))
+        # store unitname for later use
+        cache = dict({'dl_unit': self.result['dl_unit'], 'up_unit': self.result['up_unit'],
+                      'ping_unit': self.result['ping_unit']})
+        # convert unit to foreign Key
+        self.result.update({'dl_unit': self.get_or_set_unit(self.result['dl_unit']),
+                            'up_unit': self.get_or_set_unit(self.result['up_unit']),
+                            'ping_unit': self.get_or_set_unit(self.result['ping_unit'])})
+
+        ins = """INSERT INTO speedtest(ping, dl_speed, up_speed, ping_unit, dl_unit, up_unit) VALUES 
+        ({ping}, {dl_speed}, {up_speed}, {ping_unit}, {dl_unit}, {up_unit});""".format(**self.result)
 
         self.db.insert(ins)
         if DEBUG:
             print("Speedtest Finish")
-        print("Speedtest Completed")
-        print("=========================")
-        print("        Results          ")
-        print("-------------------------")
-        print("Ping = ", self.result["ping"])
-        print("Download = ", self.result["download"])
-        print("Upload = ", self.result["upload"])
-        print("=========================")
+        # restore unitname
+        self.result.update(cache)
+        self.print_msg()
 
 
 class App():
     def __init__(self):
-        usage = "usage: Speedtestcli-db v%s options" %(prog_version)
+        usage = "Speedtestcli-db v{}".format(prog_version)
         parser = OptionParser(usage=usage)
         parser.add_option("--run-test", action="store_true", default=False, dest="runtest",
                           help="run Speedtest and store result into DB")
@@ -156,7 +192,7 @@ class App():
 
             if options.createdb and not options.runtest:
                 create_new_db()
-                print("new DB "+str(DB_NAME)+" created")
+                print("new DB {} created".format(DB_NAME))
 
         except IndexError:
             Speedtest().speedtest()
